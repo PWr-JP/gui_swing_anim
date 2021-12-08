6973254fc8c28d11fc7961541ddd39dbe9129a42
Segmar <kacper.tomkiewicz@o2.pl>
2021-12-08 10:47:05 +0100

    Klasy

---
+++ src/figury/Elipsa.java
@@ -0,0 +1,65 @@
+package figury;
+
+import java.awt.*;
+import java.awt.geom.*;
+
+public class Elipsa extends Figura{
+
+    public Elipsa (Graphics2D buf, int d, int wdth, int hght ){
+        super(buf, d, wdth, hght);
+        shape = new Ellipse2D.Float(0,0,20,20);
+        aft = new AffineTransform();
+        area = new Area(shape);
+    }
+
+
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
+
+    @Override
+    public Rectangle2D getBounds2D() {
+        return null;
+    }
+
+    @Override
+    public boolean contains(double v, double v1) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Point2D point2D) {
+        return false;
+    }
+
+    @Override
+    public boolean intersects(double v, double v1, double v2, double v3) {
+        return false;
+    }
+
+    @Override
+    public boolean intersects(Rectangle2D rectangle2D) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(double v, double v1, double v2, double v3) {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Rectangle2D rectangle2D) {
+        return false;
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform affineTransform) {
+        return null;
+    }
+
+    @Override
+    public PathIterator getPathIterator(AffineTransform affineTransform, double v) {
+        return null;
+    }
+}
