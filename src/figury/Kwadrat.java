f296222978ae6534056b4baebbaf4cbf848766ca
Administrator <belhub24@gmail.com>
2021-12-15 10:53:06 +0100

    Initialize commit

---
+++ src/figury/Kwadrat.java
@@ -0,0 +1,15 @@
+package figury;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+
+public class Kwadrat extends Figura {
+    public Kwadrat(Graphics2D buffer, int delay, int width, int height) {
+        super(buffer,delay,width,height);
+        shape=new Rectangle2D.Double(0,0,30,20);
+        aft= new AffineTransform();
+        area=new Area(shape);
+    }
+}
