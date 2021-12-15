b9bc591b7fbad7f71c740c6f5da277ea0e5a5d3e
goldfivve <dominika.kmiecik6@gmail.com>
2021-12-15 21:01:07 +0100

    Add blinking and interruption of threads

--- src/figury/Ellipse.java
+++ src/figury/Ellipse.java
@@ -5,13 +5,11 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.awt.geom.Ellipse2D;
 
-public class Elipsa extends Figura{
+public class Elipsa extends Figure {
     public Elipsa(Graphics2D buf, int del, int w, int h) {
         super(buf, del, w, h);
         shape = new Ellipse2D.Float(0, 0, 10, 10);
         aft = new AffineTransform();
         area = new Area(shape);
     }
-
-
 }
--- src/figury/RoundRectangle.java
+++ src/figury/RoundRectangle.java
@@ -5,10 +5,10 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Area;
 import java.awt.geom.RoundRectangle2D;
 
-public class ZaokraglonyProstokat extends Figura{
+public class ZaokraglonyProstokat extends Figure {
     public ZaokraglonyProstokat(Graphics2D buf, int del, int w, int h) {
         super(buf, del, w, h);
-        shape = new RoundRectangle2D.Float();
+        shape = new RoundRectangle2D.Float(0, 0, 10, 10, 5, 5);
         aft = new AffineTransform();
         area = new Area(shape);
     }
