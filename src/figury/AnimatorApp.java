a5f534fd81363747b835821b876a7ca1d411bcc9
Piekarz <jakubp2000@gmail.com>
2021-12-16 03:55:27 +0100

    Dodano przyciski

--- src/figury/AnimatorApp.java
+++ src/figury/AnimatorApp.java
@@ -1,13 +1,11 @@
 package figury;
 
-import java.awt.EventQueue;
-import java.awt.Toolkit;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.JButton;
-import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -42,14 +40,14 @@ public class AnimatorApp extends JFrame {
 	public AnimatorApp() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		int ww = 450, wh = 300;
+		int ww = 750, wh = 500;
 		setBounds((screen.width-ww)/2, (screen.height-wh)/2, ww, wh);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		AnimPanel kanwa = new AnimPanel();
-		kanwa.setBounds(10, 11, 422, 219);
+		kanwa.setBounds(10, 11, 700, 400);
 		contentPane.add(kanwa);
 		SwingUtilities.invokeLater(new Runnable() {
 			
@@ -65,7 +63,7 @@ public class AnimatorApp extends JFrame {
 				kanwa.addFig();
 			}
 		});
-		btnAdd.setBounds(100, 239, 80, 23);
+		btnAdd.setBounds(10, 420, 80, 23);
 		contentPane.add(btnAdd);
 
 		JButton btnKw = new JButton("Kwadrat");
@@ -74,7 +72,7 @@ public class AnimatorApp extends JFrame {
 				kanwa.addKwadrat();
 			}
 		});
-		btnKw.setBounds(200, 239, 80, 23);
+		btnKw.setBounds(110, 420, 80, 23);
 		contentPane.add(btnKw);
 
 		JButton btnEl = new JButton("Elipsa");
@@ -83,17 +81,28 @@ public class AnimatorApp extends JFrame {
 				kanwa.addElipsa();
 			}
 		});
-		btnEl.setBounds(300, 239, 80, 23);
+		btnEl.setBounds(210, 420, 80, 23);
 		contentPane.add(btnEl);
 		
 		JButton btnAnimate = new JButton("Animate");
 		btnAnimate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				kanwa.animate();
+
 			}
 		});
-		btnAnimate.setBounds(10, 239, 80, 23);
+		btnAnimate.setBounds(410, 420, 80, 23);
 		contentPane.add(btnAnimate);
+
+
+		JButton btnCzysc = new JButton("Czyść");
+		btnCzysc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				kanwa.repaint();
+			}
+		});
+		btnCzysc.setBounds(310, 420, 80, 23);
+		contentPane.add(btnCzysc);
 		
 	}
 
