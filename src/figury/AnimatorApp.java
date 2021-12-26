4508e17e61c33e1765c5f87a5287dce59de60b72
barto-117 <bartoszkipa@gmail.com>
2021-12-26 01:33:25 +0100

    slider, który decyduje o tym, ile figur tworzy przycisk add

--- src/figury/AnimatorApp.java
+++ src/figury/AnimatorApp.java
@@ -3,10 +3,7 @@ package figury;
 import java.awt.EventQueue;
 import java.awt.Toolkit;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.JButton;
+import javax.swing.*;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -42,7 +39,7 @@ public class AnimatorApp extends JFrame {
 	public AnimatorApp() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-		int ww = 450, wh = 300;
+		int ww = 450, wh = 330;
 		setBounds((screen.width-ww)/2, (screen.height-wh)/2, ww, wh);
 		contentPane = new JPanel();
 		setContentPane(contentPane);
@@ -59,13 +56,23 @@ public class AnimatorApp extends JFrame {
 			}
 		});
 
+		JSlider slide = new JSlider(1,4,1);
+		slide.setBounds(190, 239, 200, 50);
+		slide.setMajorTickSpacing(1);
+		slide.setMinorTickSpacing(0);
+		slide.setPaintTicks(true);
+		slide.setPaintLabels(true);
+		contentPane.add(slide);
+
 		JButton btnAdd = new JButton("Add");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				kanwa.addFig();
+				for(int i = 0; i < slide.getValue(); i++){
+					kanwa.addFig();
+				}
 			}
 		});
-		btnAdd.setBounds(10, 239, 80, 23);
+		btnAdd.setBounds(10, 239, 80, 50);
 		contentPane.add(btnAdd);
 		
 		JButton btnAnimate = new JButton("Animate");
@@ -74,9 +81,8 @@ public class AnimatorApp extends JFrame {
 				kanwa.animate();
 			}
 		});
-		btnAnimate.setBounds(100, 239, 80, 23);
+		btnAnimate.setBounds(100, 239, 80, 50);
 		contentPane.add(btnAnimate);
-		
 	}
 
 }
