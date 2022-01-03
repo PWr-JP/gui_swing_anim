7b6bfb390570953b2db3301fb76fd96d6fea61fb
Yanoshek <ignaszakjanusz@gmail.com>
2022-01-03 10:53:42 +0100

    :sparkles: Added Square figure

---
+++ src/figury/Kwadrat.java
@@ -0,0 +1,17 @@
+package figury;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+
+public class Kwadrat extends Figura {
+
+	public Kwadrat(Graphics2D buf, int del, int w, int h) {
+		super(buf, del, w, h);
+		shape = new Rectangle2D.Float(0, 0, 10, 10);
+		aft = new AffineTransform();
+		area = new Area(shape);
+	}
+
+}
