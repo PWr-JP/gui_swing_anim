47257efbabc1dc77a9d076f5207a3bc661b5c269
Stanisław_Strauchold <73431130+Stachson@users.noreply.github.com>
2022-01-04 12:13:03 +0100

    Add files via upload

---
+++ src/figury/Kwadrat.java
@@ -0,0 +1,22 @@
+package figury;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+
+
+public class Kwadrat extends Figura
+{
+    public Kwadrat(Graphics2D buf, int del, int w, int h)
+    {
+        super(buf,del,w,h);
+
+        shape = new Rectangle2D.Float(0,0,20,20);
+        aft = new AffineTransform();
+        area = new Area(shape);
+
+    }
+
+
+}
