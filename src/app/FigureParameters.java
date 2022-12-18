3a84bf4f97a7fe2efec24a212946987cfa9a90c1
Quarol <karol.charchut@gmail.com>
2022-12-18 12:17:31 +0100

    1. Repo reorganized 2. Necessary classes added

---
+++ src/app/FigureParameters.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.awt.Color;
+
+public class FigureParameters {
+    public static int width = 30;
+    public static int heigth = 30;
+    public static Color color = Color.GREEN;
+
+    private FigureParameters() {}
+}
---
+++ src/app/figury/Elipsa.java
@@ -0,0 +1,9 @@
+package app.figury;
+
+import java.awt.*;
+
+public class Elipsa extends Figura{
+    public Elipsa(Graphics2D buf, int del, int w, int h) {
+        super(buf, del, w, h);
+    }
+}
